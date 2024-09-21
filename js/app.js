06b03a5f6181d5eeb83cd162519266d5d1cbaf51
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-22 01:13:20 +0500

    js

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// 1-masala
+
+// function sayHello(str) {
+//   return "hello " + str;
+// }
+
+// let name = "World";
+
+// console.log(sayHello(name));
+
+// 2-masala
+
+// function reverseSeq(n) {
+
+//     for(let i = n ; i > 0 ; i--)
+//         console.log(i);
+
+// }
+
+// let n = 5;
+
+// console.log(reverseSeq(n));
+
+//3-masala
+
+// function maxFuction(arr){
+
+//     let katta=[0];
+
+//     for(let i = 0 ; i < arr.length ; i++){
+//        katta =  Math.max(katta,arr[i]);
+//     }
+//     return katta
+// }
+
+// function minFuction(arr) {
+//         let kichik = [0];
+
+//   for (let i = 0; i < arr.length; i++) {
+//        kichik = Math.min(kichik,arr[i]);
+//   }
+//   return kichik
+// }
+
+// let arr = [-1,0,1,2,3,4,5]
+
+// console.log(maxFuction(arr));
+// console.log(minFuction(arr));
