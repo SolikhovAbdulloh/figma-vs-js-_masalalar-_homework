fdc8b4466b903fdc5705d3c0cf152bb9fc697879
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-23 09:16:27 +0500

    jl

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // 1-masala
 
+// const { reverse } = require("mz/dns");
+
+// const { name } = require("commander");
+
 // function sayHello(str) {
 //   return "hello " + str;
 // }
@@ -16,6 +20,7 @@
 //         console.log(i);
 
 // }
+
 
 // let n = 5;
 
@@ -46,3 +51,65 @@
 
 // console.log(maxFuction(arr));
 // console.log(minFuction(arr));
+
+
+// function birinchi() {
+//   console.log("Birinchi funksiya");
+//   uchinchi();
+// }
+
+// function ikkinchi() {
+//   console.log("Ikkinchi funksiya");
+  
+// }
+// function uchinchi(){
+//   console.log("uchinchi funksiya");
+// ikkinchi()
+// }
+
+// uchinchi();
+
+
+// const numbers = [1, 2, 3, 4];
+// const [first, second, third] = numbers;
+
+// console.log(first);  // 1
+// console.log(second); // 2
+// console.log(third); // 3
+
+// const arr1 = [1, 2, 3,4,5,6,7];
+// const arr2 = [...arr1, 4, 5];
+
+// console.log(arr2); // [1, 2, 3, 4, 5]
+
+// let obj = {
+//     name : 'ali',
+    
+//     age: 12,
+
+//     surname : 'aliyev',
+// }
+
+// let arr2 = {
+//     ...obj,
+//     a:1,
+//     b:2,
+// }
+
+// console.log(arr2);
+
+// const [a, ...rest] = [1, 2, 3, 4];
+
+// console.log(a); // 1
+// console.log(rest);  // [2, 3, 4]
+
+
+// const [a,...num] = [1,2,3,4,5,'ali'] 
+
+// num.splice(1,3)
+
+// console.log(a);
+
+// console.log(num);
+
+
